f369637d6124487aad3adc3996edb974b91b8b67
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-04-17 10:48:05 +0300

    style: добавить стили и разметку для компонента string

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import { ChangeEvent, FC, useState } from "react";
+import style from "./string.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
 
-export const StringComponent: React.FC = () => {
+export const StringComponent: FC = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
   return (
-    <SolutionLayout title="Строка">
-     
+    <SolutionLayout title="Строка" extraClass={style.container}>
+      <form className={style.form}>
+        <Input
+          maxLength={11}
+          isLimitText
+          onChange={onChange}
+          value={inputValue}
+        />
+        <Button text="Развернуть" isLoader />
+      </form>
+      <ul className={style.symbolList}>
+        <Circle />
+      </ul>
     </SolutionLayout>
   );
 };
