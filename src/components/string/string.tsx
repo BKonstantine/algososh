c92790cb76f5e49ce669d836c17d8510db0e74d9
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-04-17 11:27:41 +0300

    feat: добавить функцию onSubmit

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,4 +1,5 @@
-import { ChangeEvent, FC, useState } from "react";
+import { ChangeEvent, FC, FormEvent, useState } from "react";
+import { nanoid } from "nanoid";
 import style from "./string.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
@@ -7,24 +8,36 @@ import { Circle } from "../ui/circle/circle";
 
 export const StringComponent: FC = () => {
   const [inputValue, setInputValue] = useState("");
+  const [array, setArray] = useState<Array<string>>();
+  const [loader, setLoader] = useState(false);
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   };
 
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const newArray = inputValue.split("");
+    setArray(newArray);
+    setLoader(true);
+  };
+
   return (
     <SolutionLayout title="Строка" extraClass={style.container}>
-      <form className={style.form}>
+      <form className={style.form} onSubmit={onSubmit}>
         <Input
           maxLength={11}
           isLimitText
           onChange={onChange}
           value={inputValue}
         />
-        <Button text="Развернуть" isLoader />
+        <Button text="Развернуть" isLoader={loader} type="submit" />
       </form>
       <ul className={style.symbolList}>
-        <Circle />
+        {array?.map((item) => (
+          <Circle key={nanoid()} letter={item} />
+        ))}
       </ul>
     </SolutionLayout>
   );
