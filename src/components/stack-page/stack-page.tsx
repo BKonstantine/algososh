7706290572a9ea6b294fb43378f999f190635652
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-04-17 20:49:06 +0300

    style: добавить разметку и стили для компонента stack-page

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,10 +1,36 @@
-import React from "react";
+import { ChangeEvent, FC, useState } from "react";
+import style from "./stack-page.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { stack } from "./Stack";
 
-export const StackPage: React.FC = () => {
+export const StackPage: FC = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <SolutionLayout title="Стек">
-      
+      <div className={style.form}>
+        <Input
+          maxLength={4}
+          isLimitText
+          onChange={onChange}
+          value={inputValue}
+        />
+        <Button text="Добавить" type="button" disabled={!!!inputValue} />
+        <Button text="Удалить" type="button" disabled={false} />
+        <Button
+          text="Очистить"
+          type="button"
+          disabled={false}
+          extraClass={style.button}
+        />
+      </div>
+      <ul className={style.symbolList}></ul>
     </SolutionLayout>
   );
 };
