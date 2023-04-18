1a82f0674c14516289e686ee377cc1c555d4c7bb
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-04-18 10:36:09 +0300

    feat: добавить возможность добавление, удаления и очистки стека

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,15 +1,39 @@
 import { ChangeEvent, FC, useState } from "react";
+import { nanoid } from "nanoid";
 import style from "./stack-page.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { stack } from "./Stack";
+import { Circle } from "../ui/circle/circle";
 
 export const StackPage: FC = () => {
   const [inputValue, setInputValue] = useState("");
+  const [array, setArray] = useState<string[]>();
+
+  console.log(array);
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
+  };
+
+  const addElement = () => {
+    console.log("add");
+    stack.push(inputValue);
+    setArray([...stack.getElements()]);
+    setInputValue("");
+  };
+
+  const deleteElement = () => {
+    console.log("delete");
+    stack.pop();
+    setArray([...stack.getElements()]);
+  };
+
+  const clearElements = () => {
+    console.log("clear");
+    stack.clear();
+    setArray([...stack.getElements()]);
   };
 
   return (
@@ -21,16 +45,31 @@ export const StackPage: FC = () => {
           onChange={onChange}
           value={inputValue}
         />
-        <Button text="Добавить" type="button" disabled={!!!inputValue} />
-        <Button text="Удалить" type="button" disabled={false} />
+        <Button
+          text="Добавить"
+          type="button"
+          disabled={!!!inputValue}
+          onClick={addElement}
+        />
+        <Button
+          text="Удалить"
+          type="button"
+          disabled={false}
+          onClick={deleteElement}
+        />
         <Button
           text="Очистить"
           type="button"
           disabled={false}
           extraClass={style.button}
+          onClick={clearElements}
         />
       </div>
-      <ul className={style.symbolList}></ul>
+      <ul className={style.symbolList}>
+        {array?.map((element) => {
+          return <Circle key={nanoid()} letter={element} />;
+        })}
+      </ul>
     </SolutionLayout>
   );
 };
