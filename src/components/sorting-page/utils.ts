be6b79fe7633d0c31ccb388d0a0d16b91b728339
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-04-19 12:57:03 +0300

    feat: добавить функцию сортировки выбором массива

--- src/components/sorting-page/utils.ts
+++ src/components/sorting-page/utils.ts
@@ -1,4 +1,5 @@
 import { ElementStates, SortTypes } from "../../types/element-states";
+import { Direction } from "../../types/direction";
 
 export function randomArr(): SortTypes[] {
   const minLen = 3;
@@ -14,3 +15,39 @@ export function randomArr(): SortTypes[] {
 
   return arr;
 }
+
+const swap = (value: SortTypes[], firstItem: number, secondItem: number) => {
+  return ([value[firstItem], value[secondItem]] = [
+    value[secondItem],
+    value[firstItem],
+  ]);
+};
+
+export const selectionSort = (arr: SortTypes[], /* direction: Direction */) => {
+  const { length } = arr;
+  for (let i = 0; i < length - 1; i++) {
+    let maxInd = i;
+    for (let j = i + 1; j < length; j++) {
+      if (arr[maxInd] < arr[j]) {
+        maxInd = j;
+      }
+    }
+    /* if (direction === Direction.Ascending) {
+      for (let j = i + 1; j < length; j++) {
+        if (arr[maxInd] < arr[j]) {
+          maxInd = j;
+        }
+      }
+    } else {
+      for (let j = i + 1; j < length; j++) {
+        if (arr[maxInd] > arr[j]) {
+          maxInd = j;
+        }
+      }
+    } */
+
+    if (maxInd !== i) {
+      swap(arr, maxInd, i);
+    }
+  }
+};
