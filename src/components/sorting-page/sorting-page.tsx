85e82909f8f20dfe21fe6662fa3c6bfb688fd9d4
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-04-19 11:23:45 +0300

    style: добавить разметку и стили для sorting-page

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,8 +1,24 @@
 import { FC } from "react";
+import style from "./sorting-page.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { RadioInput } from "../ui/radio-input/radio-input";
+import { Button } from "../ui/button/button";
+import { Direction } from "../../types/direction";
 
 export const SortingPage: FC = () => {
-  return <SolutionLayout title="Сортировка массива">
-    
-  </SolutionLayout>;
+  return (
+    <SolutionLayout title="Сортировка массива">
+      <div className={style.form}>
+        <div className={style.form__radioButtons}>
+          <RadioInput label="Выбор" name="radio" />
+          <RadioInput label="Пузырёк" name="radio" />
+        </div>
+        <div className={style.form__buttons}>
+          <Button text="По возрастанию" sorting={Direction.Ascending} />
+          <Button text="По убыванию" sorting={Direction.Descending} />
+          <Button text="Новый массив" />
+        </div>
+      </div>
+    </SolutionLayout>
+  );
 };
