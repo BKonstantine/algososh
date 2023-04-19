314d333069a0d44b7200c7f972ce32758681a6a3
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-04-19 13:21:21 +0300

    refactor: изменить функции сортировки выбором и добавить функцию пузырьковой сортировки

--- src/components/sorting-page/utils.ts
+++ src/components/sorting-page/utils.ts
@@ -4,15 +4,12 @@ import { Direction } from "../../types/direction";
 export function randomArr(): SortTypes[] {
   const minLen = 3;
   const maxLen = 17;
-
   const len = Math.floor(Math.random() * (maxLen - minLen + 1)) + minLen;
   const arr: SortTypes[] = [];
-
   for (let i = 0; i < len; i++) {
     const randInt = Math.floor(Math.random() * 101);
     arr.push({ index: randInt, state: ElementStates.Default });
   }
-
   return arr;
 }
 
@@ -23,16 +20,11 @@ const swap = (value: SortTypes[], firstItem: number, secondItem: number) => {
   ]);
 };
 
-export const selectionSort = (arr: SortTypes[], /* direction: Direction */) => {
+export const selectionSort = (arr: SortTypes[], direction: Direction) => {
   const { length } = arr;
   for (let i = 0; i < length - 1; i++) {
     let maxInd = i;
-    for (let j = i + 1; j < length; j++) {
-      if (arr[maxInd] < arr[j]) {
-        maxInd = j;
-      }
-    }
-    /* if (direction === Direction.Ascending) {
+    if (direction === Direction.Ascending) {
       for (let j = i + 1; j < length; j++) {
         if (arr[maxInd] < arr[j]) {
           maxInd = j;
@@ -44,7 +36,7 @@ export const selectionSort = (arr: SortTypes[], /* direction: Direction */) => {
           maxInd = j;
         }
       }
-    } */
+    }
 
     if (maxInd !== i) {
       swap(arr, maxInd, i);
