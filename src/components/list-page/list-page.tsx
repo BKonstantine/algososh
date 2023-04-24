56da76f1190f8c6b76dfb417cf9a07ef1beabf1e
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-04-24 20:01:13 +0300

    feat: добавить функцию onChange и исправить стили

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,4 +1,5 @@
-import { FC, useEffect, useState } from "react";
+import { ChangeEvent, FC, useEffect, useState } from "react";
+import { nanoid } from "nanoid";
 import style from "./list-page.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
@@ -12,17 +13,31 @@ import { Circle } from "../ui/circle/circle";
 
 export const ListPage: FC = () => {
   const [array, setArray] = useState<ElementTypes[]>();
+  const [inputValue, setInputValue] = useState({
+    value: "",
+    index: 0,
+  });
 
   useEffect(() => {
     makeLinkedList();
     setArray(linkedList.getArray());
   }, []);
 
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue({ ...inputValue, [e.target.name]: e.target.value });
+  };  
+
   return (
     <SolutionLayout title="Связный список">
       <div className={style.form}>
         <div className={style.form__container}>
-          <Input maxLength={4} isLimitText placeholder="Введите значение" />
+          <Input
+            maxLength={4}
+            isLimitText
+            placeholder="Введите значение"
+            name="value"
+            onChange={onChange}
+          />
           <Button
             type="button"
             text="Добавить в head"
@@ -45,7 +60,12 @@ export const ListPage: FC = () => {
           />
         </div>
         <div className={style.form__container}>
-          <Input type="number" placeholder="Введите индекс" />
+          <Input
+            type="number"
+            placeholder="Введите индекс"
+            name="index"
+            onChange={onChange}
+          />
           <Button
             type="button"
             text="Добавить по индексу"
@@ -59,18 +79,31 @@ export const ListPage: FC = () => {
         </div>
       </div>
       <ul className={style.symbolList}>
-        <li className={style.symbolList__item}>
-          <Circle
-            isSmall
-            extraClass={`${style.circle} ${style.circle__type_add}`}
-          />
-          <Circle extraClass={style.circle} index={0} />
-          <Circle
-            isSmall
-            extraClass={`${style.circle} ${style.circle__type_delete}`}
-          />
-          <ArrowIcon />
-        </li>
+        {array?.map((item, index) => {
+          return (
+            <li className={style.symbolList__item} key={nanoid()}>
+              {false && (
+                <Circle
+                  isSmall
+                  extraClass={`${style.circle} ${style.circle__type_add}`}
+                />
+              )}
+              <Circle
+                extraClass={style.circle}
+                index={index}
+                letter={item.letter}
+                state={item.state}
+              />
+              {false && (
+                <Circle
+                  isSmall
+                  extraClass={`${style.circle} ${style.circle__type_delete}`}
+                />
+              )}
+              {true && <ArrowIcon />}
+            </li>
+          );
+        })}
       </ul>
     </SolutionLayout>
   );
