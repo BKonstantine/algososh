5478c270c4da3293926128a5406d0f3b9f993473
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-05-17 18:59:51 +0300

    feat: добавить три е2е теста для связанного списка

---
+++ cypress/e2e/list.spec.cy.js
@@ -0,0 +1,131 @@
+import {
+  circleItem,
+  circleContent,
+  circleSmall,
+  stateChanging,
+  stateDefault,
+  stateModified,
+  inputValue,
+  inputIndex,
+  addToHeadButton,
+  addToTailButton,
+  deleteFromHeadButton,
+  deleteFromTailButton,
+  addByIndexButton,
+  deleteByIndexButton,
+} from "../variables";
+import { SHORT_DELAY_IN_MS } from "../../src/constants/delays";
+
+const getDataFromCircle = (array) => {
+  cy.get(circleItem).then((item) => {
+    cy.get(item)
+      .children()
+      .each((el) => {
+        array.push(el.text().trim());
+      });
+  });
+};
+
+describe("Проверка визуализации структуры данных 'Связанный список'", () => {
+  beforeEach(() => {
+    cy.viewport(1440, 900);
+    cy.visit("http://localhost:3000/list");
+  });
+
+  it("Начальное состояние страницы отображается корректно", () => {
+    cy.get(inputValue).should("have.value", "");
+    cy.get(inputIndex).should("have.value", "");
+    cy.get(addToHeadButton).should("be.disabled");
+    cy.get(addToTailButton).should("be.disabled");
+    cy.get(deleteFromHeadButton).should("not.be.disabled");
+    cy.get(deleteFromTailButton).should("not.be.disabled");
+    cy.get(addByIndexButton).should("be.disabled");
+    cy.get(deleteByIndexButton).should("be.disabled");
+  });
+
+  it("Удаление элемента из head работает корректно", () => {
+    let circleData = [];
+    getDataFromCircle(circleData);
+    cy.get(deleteFromHeadButton).should("not.be.disabled");
+    cy.get(deleteFromHeadButton).click();
+    cy.get(deleteFromHeadButton)
+      .invoke("attr", "class")
+      .then((classList) => expect(classList).contains("loader"));
+    cy.get(circleContent).then((item) => {
+      cy.get(item[0])
+        .children()
+        .then((item) => {
+          cy.get(item[1]).children().should("have.text", "");
+        });
+    });
+    cy.get(circleContent).then((item) => {
+      cy.get(item[0])
+        .find(circleSmall)
+        .invoke("attr", "class")
+        .then((classList) => expect(classList).contains(stateChanging));
+      cy.get(item[0])
+        .find(circleSmall)
+        .children()
+        .should("have.text", circleData[0]);
+    });
+    cy.wait(SHORT_DELAY_IN_MS);
+  });
+
+  it("Удаление элемента из tail работает корректно", () => {
+    let circleData = [];
+    getDataFromCircle(circleData);
+    cy.get(deleteFromTailButton).should("not.be.disabled");
+    cy.get(deleteFromTailButton).click();
+    cy.get(deleteFromTailButton)
+      .invoke("attr", "class")
+      .then((classList) => expect(classList).contains("loader"));
+    cy.get(circleContent).then((item) => {
+      cy.get(item[circleData.length - 1])
+        .children()
+        .then((item) => {
+          cy.get(item[1]).children().should("have.text", "");
+        });
+    });
+    cy.get(circleContent).then((item) => {
+      cy.get(item[circleData.length - 1])
+        .find(circleSmall)
+        .invoke("attr", "class")
+        .then((classList) => expect(classList).contains(stateChanging));
+      cy.get(item[circleData.length - 1])
+        .find(circleSmall)
+        .children()
+        .should("have.text", circleData[circleData.length - 1]);
+    });
+    cy.wait(SHORT_DELAY_IN_MS);
+  });
+
+  it("Добавление элемента в head работает корректно", () => {
+    const number = "1";
+    cy.get(inputValue).type(number);
+    cy.get(addToHeadButton).should("not.be.disabled");
+    cy.get(addToHeadButton).click();
+    cy.get(addToHeadButton)
+      .invoke("attr", "class")
+      .then((classList) => expect(classList).contains("loader"));
+    cy.get(circleContent).then((item) => {
+      cy.get(item[0])
+        .find(circleSmall)
+        .invoke("attr", "class")
+        .then((classList) => expect(classList).contains(stateChanging));
+      cy.get(item[0]).find(circleSmall).children().should("have.text", number);
+    });
+    cy.wait(SHORT_DELAY_IN_MS);
+    cy.get(circleItem).then((item) => {
+      cy.get(item[0])
+        .invoke("attr", "class")
+        .then((classList) => expect(classList).contains(stateModified));
+      cy.get(item[0]).children().should("have.text", number);
+    });
+    cy.wait(SHORT_DELAY_IN_MS);
+    cy.get(circleItem).then((item) => {
+      cy.get(item[0])
+        .invoke("attr", "class")
+        .then((classList) => expect(classList).contains(stateDefault));
+    });
+  });
+});
--- cypress/variables.js
+++ cypress/variables.js
@@ -1,6 +1,7 @@
 export const input = "input";
 export const circleContent = '[class^="circle_content"]';
 export const circleItem = '[class^="circle_circle"]';
+export const circleSmall = '[class*="circle_small"]';
 export const stateModified = "circle_modified";
 export const stateDefault = "circle_default";
 export const stateChanging = "circle_changing";
@@ -15,3 +16,12 @@ export const radioInputLabels = "label";
 export const addButton = "[data='add-button']";
 export const deleteButton = "[data='delete-button']";
 export const clearButton = "[data='clear-button']";
+
+export const inputValue = "[data='input-value']";
+export const inputIndex = "[data='input-index']";
+export const addToHeadButton = "[data='add-to-head-button']";
+export const addToTailButton = "[data='add-to-tail-button']";
+export const deleteFromHeadButton = "[data='delete-from-head-button']";
+export const deleteFromTailButton = "[data='delete-from-tail-button']";
+export const addByIndexButton = "[data='add-by-index-button']";
+export const deleteByIndexButton = "[data='delete-by-index-button']";
